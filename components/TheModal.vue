1e9e0fd2e74c27bd7f0b1220840f2aab5a573a01
rafaeltj13 <rafael.damaciel@gmail.com>
2023-05-20 14:06:28 -0300

    feat: added animation to modal

--- components/TheModal.vue
+++ components/TheModal.vue
@@ -1,9 +1,9 @@
 <template>
-  <div @click="openDialog = true">
+  <div @click="toggle(true)">
     <slot />
   </div>
   <div
-    v-if="openDialog"
+    v-show="openDialog"
     class="relative z-10"
     aria-labelledby="modal-title"
     role="dialog"
@@ -15,15 +15,18 @@
 
     <div class="fixed inset-0 z-10 overflow-y-auto">
       <div
-        class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+        :class="{
+          '!opacity-100 !scale-100': displayModal,
+        }"
+        class="transition-all opacity-0 scale-0 duration-300 flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
       >
         <div
-          class="relative transform overflow-hidden rounded-2xl bg-background dark:bg-background-dark text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+          class="relative overflow-hidden rounded-2xl bg-background dark:bg-background-dark shadow-xl sm:my-8 sm:w-full sm:max-w-lg dark:border-2 dark:border-primary-dark"
         >
           <TheIcon
             customClass="absolute text-primary dark:text-primary-dark hover:scale-110 transition-transform cursor-pointer top-5 right-5 h-5 w-5"
             faIcon="fa-solid fa-close"
-            @click="() => (openDialog = false)"
+            @click="() => toggle(false)"
           />
           <div
             class="bg-background dark:bg-background-dark px-4 pb-4 pt-5 sm:p-6 sm:pb-4"
@@ -38,11 +41,20 @@
 
 <script setup lang="ts">
 const openDialog = ref(false);
+const displayModal = ref(false);
+
+const toggle = (value: boolean) => {
+  openDialog.value = value;
+
+  setTimeout(() => {
+    displayModal.value = value;
+  }, 100);
+};
 
 onMounted(() => {
   window.onkeydown = function (event) {
     if (event.key === "Escape") {
-      openDialog.value = false;
+      toggle(false);
     }
   };
 });
--- error.vue
+++ error.vue
@@ -1,3 +1,7 @@
+<template>
+  <div>Error page</div>
+</template>
+
 <script setup lang="ts">
 navigateTo("/");
 </script>
