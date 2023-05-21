899e8b836e539ff527cc7ac65bc4982c3dff6296
rafaeltj13 <rafael.damaciel@gmail.com>
2023-05-21 18:11:52 -0300

    fix: animation for notification

--- components/TheNotification.vue
+++ components/TheNotification.vue
@@ -1,26 +1,22 @@
 <template>
-  <div v-show="notification && notification.title">
-    <div
-      :class="{
-        '!translate-y-24': displayNotification,
-      }"
-      class="w-[350px] absolute translate-y-0 top-0 transition duration-500 left-0 right-0 mx-auto bg-sidebar dark:bg-sidebar-dark dark:border dark:border-primary shadow-xl rounded-lg"
-    >
-      <div class="flex items-center justify-between p-4">
-        <div>
-          <p class="text-primary dark:text-primary-dark">
-            {{ notification?.title }}
-          </p>
-          <p class="text-secondary dark:text-secondary-dark">
-            {{ notification?.content }}
-          </p>
-        </div>
-        <TheIcon
-          customClass="text-primary dark:text-primary-dark hover:scale-110 transition-transform cursor-pointer top-5 right-5 h-5 w-5"
-          faIcon="fa-solid fa-close"
-          @click="() => setNotification(null)"
-        />
+  <div
+    :class="{ 'translate-y-24 ': notification && notification.title }"
+    class="w-[350px] absolute top-0 transition duration-300 left-0 right-0 mx-auto bg-sidebar dark:bg-sidebar-dark border border-primary dark:border-primary shadow-xl rounded-lg"
+  >
+    <div class="flex items-center justify-between p-4">
+      <div>
+        <p class="text-primary dark:text-primary-dark">
+          {{ notification?.title }}
+        </p>
+        <p class="text-secondary dark:text-secondary-dark">
+          {{ notification?.content }}
+        </p>
       </div>
+      <TheIcon
+        customClass="text-primary dark:text-primary-dark hover:scale-110 transition-transform cursor-pointer top-5 right-5 h-5 w-5"
+        faIcon="fa-solid fa-close"
+        @click="() => setNotification(null)"
+      />
     </div>
   </div>
 </template>
